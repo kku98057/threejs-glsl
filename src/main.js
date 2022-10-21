869e0365d8b428604bd9d96dab5e333e15313a49
DESKTOP-870CDBC\SUNHU <kku98057@naver.com>
2022-10-21 18:52:38 +0900

    raycasting

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import * as THREE from "three";
 import vertex from "../src/shaders/vertex.glsl";
 import fragment from "../src/shaders/fragment.glsl";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import t1 from "../src/asset/img/duck/DuckCM.png";
-import mask from "../src/asset/img/circle.png";
+import t1 from "../src/asset/img/eco.png";
+import t2 from "../src/asset/img/loopy.png";
+import mask from "../src/asset/img/fox/Texture.png";
+import { gsap } from "gsap";
 // init
+
+//https://www.youtube.com/watch?v=8K5wJeVgjrM
+
+// 17분34초
 
 export default class Sketch {
   constructor() {
@@ -18,20 +24,71 @@ export default class Sketch {
     this.container = document.querySelector(".s1");
     this.scene = new THREE.Scene();
 
+    this.raycaster = new THREE.Raycaster();
+    this.mouse = new THREE.Vector2();
+    this.points = new THREE.Vector2();
+
     this.textures = [
       new THREE.TextureLoader().load(t1),
-      new THREE.TextureLoader().load(mask),
+      new THREE.TextureLoader().load(t2),
     ];
-
+    this.mask = new THREE.TextureLoader().load(mask);
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.container.appendChild(this.renderer.domElement);
 
-    new OrbitControls(this.camera, this.renderer.domElement);
+    // new OrbitControls(this.camera, this.renderer.domElement);
 
     this.addMesh();
+    this.light();
     this.time = 0;
+    this.move = 0;
+    this.mouseEffects();
     this.render();
+  }
+  mouseEffects() {
+    this.test = new THREE.Mesh(
+      new THREE.PlaneGeometry(window.innerWidth, window.innerHeight),
+      new THREE.MeshStandardMaterial()
+    );
+    window.addEventListener("mousedown", (e) => {
+      gsap.to(this.material.uniforms.mousePressed, {
+        duration: 1,
+        // ease: "elastic.out(1,0.3)",
+        value: 1,
+      });
+    });
+    window.addEventListener("mouseup", (e) => {
+      gsap.to(this.material.uniforms.mousePressed, {
+        duration: 1,
+        // ease: "elastic.out(1,0.3)",
+        value: 0,
+      });
+    });
+    window.addEventListener("mousewheel", (e) => {
+      this.move += e.wheelDeltaY / 1000;
+    });
+    window.addEventListener(
+      "mousemove",
+      (e) => {
+        this.mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+        this.mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+        this.raycaster.setFromCamera(this.mouse, this.camera);
+
+        let intersects = this.raycaster.intersectObjects([this.test]);
+
+        this.points.x = intersects[0].point.x;
+        this.points.y = intersects[0].point.y;
+      },
+      false
+    );
+  }
+  light() {
+    this.light = new THREE.DirectionalLight({
+      color: 0xffffff,
+      intensity: 0.5,
+    });
+    this.scene.add(this.light);
   }
   addMesh() {
     let number = 512 * 512;
@@ -42,39 +99,63 @@ export default class Sketch {
       new Float32Array(number * 3),
       3
     );
+    this.speed = new THREE.BufferAttribute(new Float32Array(number * 1), 1);
+    this.offset = new THREE.BufferAttribute(new Float32Array(number * 1), 1);
+    this.direction = new THREE.BufferAttribute(new Float32Array(number * 1), 1);
+    this.press = new THREE.BufferAttribute(new Float32Array(number * 1), 1);
+
     this.material = new THREE.ShaderMaterial({
       side: THREE.DoubleSide,
-      transparent: true,
+      transparent: false,
       depthTest: false,
       depthWrite: false,
+
       fragmentShader: fragment,
       vertexShader: vertex,
       uniforms: {
         progress: { type: "f", value: 0 },
         t1: { type: "t", value: this.textures[0] },
-        mask: { type: "t", value: this.textures[1] },
+        t1: { type: "t", value: this.textures[1] },
+        mask: { type: "t", value: this.mask },
+        mousePressed: { type: "f", value: 0 },
+        mouse: { type: "v2", value: null },
+        move: { type: "f", value: 0 },
+        time: { type: "f", value: 0 },
       },
     });
     let index = 0;
+    function rand(a, b) {
+      return a + (b - a) * Math.random();
+    }
     for (let i = 0; i < 512; i++) {
       let posX = i - 256;
       for (let j = 0; j < 512; j++) {
         this.positions.setXYZ(index, posX * 2, (j - 256) * 2, 0);
         this.coordinates.setXYZ(index, i, j, 0);
+        this.offset.setX(index, rand(-1000, 1000));
+        this.speed.setX(index, rand(0.4, 1));
+        this.direction.setX(index, Math.random() > 0.5 ? 1 : -1);
+        this.press.setX(index, rand(0.4, 1));
         index++;
       }
     }
 
     this.geometry.setAttribute("position", this.positions);
     this.geometry.setAttribute("aCoordinates", this.coordinates);
+    this.geometry.setAttribute("aSpeed", this.speed);
+    this.geometry.setAttribute("aOffset", this.offset);
+    this.geometry.setAttribute("aPress", this.press);
+    this.geometry.setAttribute("aDirection", this.direction);
 
     this.mesh = new THREE.Points(this.geometry, this.material);
     this.scene.add(this.mesh);
   }
   render() {
     this.time++;
-    // this.mesh.rotation.x += 0.01;
-    // this.mesh.rotation.y += 0.02;
+
+    this.material.uniforms.time.value = this.time;
+    this.material.uniforms.move.value = this.move;
+    this.material.uniforms.mouse.value = this.points;
     this.renderer.render(this.scene, this.camera);
     window.requestAnimationFrame(this.render.bind(this));
   }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,16 +1,42 @@
 varying vec2 vUv;
+varying vec3 vPos;
 varying vec2 vCoordinates;
 attribute vec3 aCoordinates;
+attribute float aSpeed;
+attribute float aOffset;
+attribute float aPress;
+attribute float aDirection;
 
+uniform float move;
+uniform float time;
+uniform vec2 mouse;
+uniform float mousePressed;
 
 void main(){
 
 vUv = uv;
+vec3 pos = position;
+// NOT STABLE
+pos.x += sin(move*aSpeed)*3.;
+pos.y += sin(move*aSpeed)*3.;
+pos.z = mod(position.z + move*100.*aSpeed + aOffset , 2000.) - 1000.;
 
-vec4 mvPosition = modelViewMatrix * vec4(position,1);
-gl_PointSize = 2000. * (1./ - mvPosition.z);
+
+vec3 stable = position;
+float dist = distance(stable.xy,mouse);
+float area = 1. - smoothstep(0.,150.,dist);
+
+stable.x += 50.*sin(0.2*time*aPress) * aDirection*area*mousePressed;
+stable.y += 50.*sin(0.2*time*aPress) * aDirection*area*mousePressed;
+stable.z += 200.*cos(0.2*time*aPress) * aDirection*area*mousePressed;
+
+
+// STABLE
+vec4 mvPosition = modelViewMatrix * vec4(stable,1);
+gl_PointSize = 2100. * (1./ - mvPosition.z);
 gl_Position = projectionMatrix * mvPosition;
 
 vCoordinates = aCoordinates.xy;
+vPos = pos;
 
 }
